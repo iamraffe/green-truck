1b3437db507d975844761cd0312fd5dfd83fe186
raffe90 <raffe90@gmail.com>
2015-11-03 17:45:43 +0100

    About section

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -9,52 +9,21 @@
         <meta name="author" content="">
         <link rel="stylesheet" href="{{ elixir('css/libs.css') }}">
         <link rel="stylesheet" href="{{ elixir('css/all.css') }}">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
     </head>
     <body>
-    <!-- Navigation -->
-    <nav class="navbar navbar-default navbar-fixed-bottom" role="navigation">
-        <div class="container">
-            <!-- Brand and toggle get grouped for better mobile display -->
-            <div class="navbar-header">
-                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-                    <span class="sr-only">Toggle navigation</span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                </button>
-                <a class="navbar-brand" href="#"><h1 style="display: none;">Green Truck</h1><img src="/img/green-truck-logo-low.png" alt="Green Truck Logo"></a>
-            </div>
-            <!-- Collect the nav links, forms, and other content for toggling -->
-            <div class="collapse navbar-collapse pull-right" id="bs-example-navbar-collapse-1">
-                <ul class="nav navbar-nav">
-                    <li>
-                        <a href="#">¿Quiénes somos?</a>
-                    </li>
-                    <li>
-                        <a href="#">Servicios</a>
-                    </li>
-                    <li>
-                        <a href="#">Experiencia</a>
-                    </li>
-                    <li>
-                        <a href="#">Contacto</a>
-                    </li>
-                </ul>
-            </div>
-            <!-- /.navbar-collapse -->
-        </div>
-        <!-- /.container -->
-    </nav>
+
 
     <!-- Full Page Image Background Carousel Header -->
-    <header id="myCarousel" class="carousel slide">
-
-{{--         <!-- Indicators -->
+    <header>
+        <div class="overlay"></div>
+{{-- 
+        <!-- Indicators -->
         <ol class="carousel-indicators">
             <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
             <li data-target="#myCarousel" data-slide-to="1"></li>
             <li data-target="#myCarousel" data-slide-to="2"></li>
-        </ol> --}}
+        </ol>
 
         <!-- Wrapper for Slides -->
         <div class="carousel-inner">
@@ -83,8 +52,8 @@
                     <div class="container">
                         <div class="row">
                             <div class="col-md-6 col-sm-12">
-{{--                                 <h1>Green Truck</h1>
-                                <h2>Nos centramos en el proceso de recogida, selección y trituración de todo tipo de residuos y restos de madera, para que posteriormente puedan ser reutilizados.</h2> --}}
+                                <h1>Green Truck</h1>
+                                <h2>Nos centramos en el proceso de recogida, selección y trituración de todo tipo de residuos y restos de madera, para que posteriormente puedan ser reutilizados.</h2>
                             </div>
                         </div>
                         <!-- /.row -->
@@ -100,20 +69,20 @@
                     <div class="container">
                         <div class="row">
                             <div class="col-md-6 col-sm-12">
-{{--                                 <h1>Green Truck</h1> --}}
+                                <h1>Green Truck</h1>
                                 <h2>Nos centramos en el proceso de recogida, selección y trituración de todo tipo de residuos y restos de madera, para que posteriormente puedan ser reutilizados.</h2>
                             </div>
                         </div>
                         <!-- /.row -->
                     </div><!-- /.container -->
                 </div>
-{{--                 <div class="carousel-caption">
+                <div class="carousel-caption">
                     <h2>Caption 3</h2>
-                </div> --}}
+                </div>
             </div>
         </div>
 
-{{--         <!-- Controls -->
+        <!-- Controls -->
         <a class="left carousel-control" href="#myCarousel" data-slide="prev">
             <span class="icon-prev"></span>
         </a>
@@ -121,14 +90,84 @@
             <span class="icon-next"></span>
         </a> --}}
     </header>
-
+    <!-- Navigation -->
+    <nav class="navbar navbar-default" role="navigation">
+        <div class="container">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="#"><h1 style="display: none;">Green Truck</h1><img src="/img/green-truck-logo-low.png" alt="Green Truck Logo"></a>
+            </div>
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse pull-right" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav">
+                    <li>
+                        <a href="#quienes-somos">¿Quiénes somos?</a>
+                    </li>
+                    <li>
+                        <a href="#">Servicios</a>
+                    </li>
+                    <li>
+                        <a href="#">Experiencia</a>
+                    </li>
+                    <li>
+                        <a href="#">Contacto</a>
+                    </li>
+                </ul>
+            </div>
+            <!-- /.navbar-collapse -->
+        </div>
+        <!-- /.container -->
+    </nav>
+    <!-- Organizacion Section -->
+    <section id="quienes-somos" class="bg-border-green">
+{{--         <img src="img/witch.svg" class="img-responsive img-centered witch" alt="Bruja"> --}}
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12 text-center">
+                    <h2 class="section-heading animated-header">¿Quiénes somos?</h2>
+                    <h3 class="section-subheading text-muted">Somos el resultado de la aplicación de tecnología de vanguardia a la gestión de residuos, principalmente de la actividad terciaria.</h3>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-12 text-center">
+                  <div class="col-sm-4">
+                    <span class="fa-stack fa-4x fa-lg animated-fa">
+                      <i class="fa fa-circle fa-stack-2x"></i>
+                      <i class="fa fa-globe fa-stack-1x fa-inverse"></i>
+                    </span>
+                    <p>Green Truck nace en el año 1986. Estamos comprometidos con el respeto al medio ambiente y  hemos creado un riguroso proceso de retirada y transporte de residuos con equipos humanos expertos.</p>
+                  </div>
+                  <div class="col-sm-4">
+                    <span class="fa-stack fa-4x fa-lg animated-fa">
+                      <i class="fa fa-circle fa-stack-2x"></i>
+                      <i class="fa fa-tree fa-stack-1x fa-inverse"></i>
+                    </span>
+                    <p>Nuestra actividad se centra en el proceso de recogida, selección y trituración de todo tipo de residuos y restos de madera, para que posteriormente puedan ser reutilizados.</p>
+                  </div>
+                  <div class="col-sm-4">
+                    <span class="fa-stack fa-4x fa-lg animated-fa">
+                      <i class="fa fa-circle fa-stack-2x"></i>
+                      <i class="fa fa-truck fa-stack-1x fa-inverse"></i>
+                    </span>
+                    <p>En Greentruck contamos con la maquinaria necesaria para la recogida de tipo de restos y materiales sobrantes de las empresas. Más de 25 años de experiencia en el sector nos avalan.</p>
+                  </div>
+                </div>
+            </div>
+        </div>
+    </section>
     <!-- Organizacion Section -->
     <section id="organizacion" class="bg-light-gray">
 {{--         <img src="img/witch.svg" class="img-responsive img-centered witch" alt="Bruja"> --}}
         <div class="container">
             <div class="row">
                 <div class="col-lg-12 text-center">
-                    <h2 class="section-heading">¿Quiénes somos?</h2>
+                    <h2 class="section-heading animated-header">¿Quiénes somos?</h2>
                     <h3 class="section-subheading text-muted">Somos el resultado de la aplicación de tecnología de vanguardia a la gestión de residuos, principalmente de la actividad terciaria.</h3>
                 </div>
             </div>
@@ -141,7 +180,44 @@
             </div>
         </div>
     </section>
-
+    <!-- Organizacion Section -->
+    <section id="organizacion" class="bg-light-gray">
+{{--         <img src="img/witch.svg" class="img-responsive img-centered witch" alt="Bruja"> --}}
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12 text-center">
+                    <h2 class="section-heading animated-header">¿Quiénes somos?</h2>
+                    <h3 class="section-subheading text-muted">Somos el resultado de la aplicación de tecnología de vanguardia a la gestión de residuos, principalmente de la actividad terciaria.</h3>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-12 text-center">
+                  <p>Green Truck nace en el año 1986. Estamos comprometidos con el respeto al medio ambiente y  hemos creado un riguroso proceso de retirada y transporte de residuos con equipos humanos expertos.</p>
+                  <p>Nuestra actividad se centra en el proceso de recogida, selección y trituración de todo tipo de residuos y restos de madera, para que posteriormente puedan ser reutilizados.</p>
+                  <p>En Greentruck contamos con la maquinaria necesaria para la recogida de tipo de restos y materiales sobrantes de las empresas. Más de 25 años de experiencia en el sector nos avalan.</p>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- Organizacion Section -->
+    <section id="organizacion" class="bg-light-gray">
+{{--         <img src="img/witch.svg" class="img-responsive img-centered witch" alt="Bruja"> --}}
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12 text-center">
+                    <h2 class="section-heading animated-header">¿Quiénes somos?</h2>
+                    <h3 class="section-subheading text-muted">Somos el resultado de la aplicación de tecnología de vanguardia a la gestión de residuos, principalmente de la actividad terciaria.</h3>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-12 text-center">
+                  <p>Green Truck nace en el año 1986. Estamos comprometidos con el respeto al medio ambiente y  hemos creado un riguroso proceso de retirada y transporte de residuos con equipos humanos expertos.</p>
+                  <p>Nuestra actividad se centra en el proceso de recogida, selección y trituración de todo tipo de residuos y restos de madera, para que posteriormente puedan ser reutilizados.</p>
+                  <p>En Greentruck contamos con la maquinaria necesaria para la recogida de tipo de restos y materiales sobrantes de las empresas. Más de 25 años de experiencia en el sector nos avalan.</p>
+                </div>
+            </div>
+        </div>
+    </section> 
     <script src="js/libs.js"></script>
     <!-- Script to Activate the Carousel -->
     <script>
@@ -149,6 +225,38 @@
             pause: false,
             interval: 10000 //changes the speed
         })
+        /* affix the navbar after scroll below header */
+        $('nav.navbar').affix({
+              offset: {
+                top: $('header').height()-$('nav.navbar').height()
+              }
+        }); 
+
+        /* highlight the top nav as scrolling occurs */
+        $('body').scrollspy({ target: 'nav.navbar' });
+
+        $(window).scroll(function() {
+            $('.animated-header').each(function(){
+            var imagePos = $(this).offset().top;
+
+            var topOfWindow = $(window).scrollTop();
+                if (imagePos < topOfWindow+900) {
+                    console.log($(this));
+                    $(this).addClass("fadeInDown animated");
+                }
+            });
+
+            $('.animated-fa').each(function(){
+            var imagePos = $(this).offset().top;
+
+            var topOfWindow = $(window).scrollTop();
+                if (imagePos < topOfWindow+900) {
+                    console.log($(this));
+                    $(this).addClass("fadeInUp animated");
+                }
+            });
+        }); 
+
     </script>
     </body>
 </html>
